de3f2b801854150cfc039d053a44e95181606a82
Pranaya Deomani <pranayadeomani@proton.me>
2023-10-20 13:10:48 +0530

    extra: Conditionally include MindTheGapps

--- product.mk
+++ product.mk
@@ -1,3 +1,10 @@
+# GMS
+ifneq ($(LINEAGE_BUILD),)
+ifeq ($(WITH_GMS),true)
+$(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)
+endif
+endif
+
 # ih8sn
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilt/etc/ih8sn.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/ih8sn.conf
