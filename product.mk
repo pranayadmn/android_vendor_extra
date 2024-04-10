4e98ee902fc7375497e58c0db8bf944c28a00b2e
Pranaya Deomani <pranayadeomani@proton.me>
2024-04-11 00:50:57 +0530

    extra: Conditionally include vendor/gms
    Change-Id: I291f55b9619844cec7c0ad9eafd58dff2c944174

--- product.mk
+++ product.mk
@@ -1,2 +1,9 @@
+# GMS
+ifneq ($(LINEAGE_BUILD),)
+ifeq ($(WITH_GMS),true)
+$(call inherit-product, vendor/gms/common/common-vendor.mk)
+endif
+endif
+
 # Signing
 PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/certs/releasekey
